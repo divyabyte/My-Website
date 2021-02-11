0b9e01ebe87102b9c7959c8fe02648d14e0a8da1
Divya Singhal <divyasinghal@Divyas-MacBook-Air.local>
2021-02-11 18:38:54 +0530

    added blur fiter

--- insight/js/app.js
+++ insight/js/app.js
@@ -1,14 +1,39 @@
-<script>
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+    var element = document.getElementById("demo");
+    var element1 = document.getElementById("demo-blur");
+    var element2 = document.getElementById("demo1");
+    var newElement = document.getElementById("wrapper");
+
+    if(element.classList.contains("blur-filter"))   
+    {
+      element.classList.remove("blur-filter");
+      newElement.classList.remove("stop-scroll");
+      
+    } 
+    else{
+      element.classList.add("blur-filter");
+      newElement.classList.add("stop-scroll");
+    }
+
+    if(element1.classList.contains("blur-filter"))   
+    {
+      element1.classList.remove("blur-filter");
+    } 
+    else{
+      element1.classList.add("blur-filter");
+    } 
+
+    if(element2.classList.contains("blur-filter"))   
+    {
+      element2.classList.remove("blur-filter");
+    } 
+    else{
+      element2.classList.add("blur-filter");
+    }   
+
+
   }
-}
-</script>
+
+
+
+
